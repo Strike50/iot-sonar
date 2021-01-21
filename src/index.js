4f8991839169a07745e08d008f41cc2ea6ccccee
loukahoulgatte <lhoulgattebustamante@gmail.com>
2021-01-21 14:08:43 +0100

    Add apollo client

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
